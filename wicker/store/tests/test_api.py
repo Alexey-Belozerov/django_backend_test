b6623eea621dbf6179329ecb5bd3c7493abaedd6
alex <belaz.lug@yandex.ru>
2022-05-11 20:59:11 +0300

    Создана переменная для сериализации данных объектов

--- wicker/store/tests/test_api.py
+++ wicker/store/tests/test_api.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from store.models import Wicker
+from store.serializers import WickerSerializer
 
 
 class WickerApiTestCase(APITestCase):
@@ -11,4 +12,5 @@ class WickerApiTestCase(APITestCase):
         wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815)
         url = reverse('wicker-list')
         response = self.client.get(url)
+        serializer_data = WickerSerializer([wicker_1, wicker_2], many=True).data
 
