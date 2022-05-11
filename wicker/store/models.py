a4e833e17a63e8d64af034f4ff3c8fabbcc34fcb
alex <belaz.lug@yandex.ru>
2022-05-11 22:18:17 +0300

    В модель добавлено поле author_name, миграции

--- wicker/store/models.py
+++ wicker/store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Wicker(models.Model):
     name = models.CharField('Имя', max_length=255)
     price = models.DecimalField('Цена', max_digits=7, decimal_places=2)
+    author_name = models.CharField('Автор изделия', max_length=255)
 
     class Meta:
         verbose_name = 'Корзинка'
