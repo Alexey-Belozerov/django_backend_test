d3e8636fe5d7e3e1718aabc1d7dcb8f18c71570f
alex <belaz.lug@yandex.ru>
2022-05-11 21:19:25 +0300

    Создана переменная для сериализации данных объектов

--- wicker/store/tests/test_serializers.py
+++ wicker/store/tests/test_serializers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from store.models import Wicker
+from store.serializers import WickerSerializer
 
 
 class WickerSerializerTestCase(TestCase):
@@ -8,3 +9,5 @@ class WickerSerializerTestCase(TestCase):
     def test_ok(self):
         wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500)
         wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815)
+        data = WickerSerializer([wicker_1, wicker_2], many=True).data
+
