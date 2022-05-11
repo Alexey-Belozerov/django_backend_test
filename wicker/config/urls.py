9159262286614a8ff0db20e599b8a00770d3b5df
alex <belaz.lug@yandex.ru>
2022-05-11 19:42:21 +0300

    Прописали urls

--- wicker/config/urls.py
+++ wicker/config/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from store.views import WickerViewSet
+
+router = SimpleRouter()
+router.register(r'wicker', WickerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
