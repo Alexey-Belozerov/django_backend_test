3c6e5fbb15dc352177c365d6622bd2d58dcd0852
alex <belaz.lug@yandex.ru>
2022-05-11 22:38:44 +0300

    Добавлена возможность сортировки в API; фильтр, поиск, сортировка по инструкции в разделе  filtering сайта django-rest-framework.org

--- wicker/store/views.py
+++ wicker/store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Wicker
@@ -10,7 +10,8 @@ from store.serializers import WickerSerializer
 class WickerViewSet(ModelViewSet):
     queryset = Wicker.objects.all()
     serializer_class = WickerSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filter_fields = ['price']
     search_fields = ['author_name', 'price']
+    ordering_fields = ['name', 'price']
 
