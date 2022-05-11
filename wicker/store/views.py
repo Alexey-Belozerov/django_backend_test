4f820618bbc7ef6f12bc35579fe4477a878d4761
alex <belaz.lug@yandex.ru>
2022-05-11 22:27:19 +0300

    Добавлена возможность поиска в API

--- wicker/store/views.py
+++ wicker/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Wicker
@@ -9,6 +10,7 @@ from store.serializers import WickerSerializer
 class WickerViewSet(ModelViewSet):
     queryset = Wicker.objects.all()
     serializer_class = WickerSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filter_fields = ['price']
+    search_fields = ['author_name', 'price']
 
