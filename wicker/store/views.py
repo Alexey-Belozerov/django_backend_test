a80c65bb195f08dc60714e0d1b6cecd1cea184e4
alex <belaz.lug@yandex.ru>
2022-05-11 22:04:18 +0300

    Добавлена возможность фильтрации в API, установлен формат json данных по умолчанию

--- wicker/store/views.py
+++ wicker/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Wicker
@@ -8,4 +9,6 @@ from store.serializers import WickerSerializer
 class WickerViewSet(ModelViewSet):
     queryset = Wicker.objects.all()
     serializer_class = WickerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['price']
 
