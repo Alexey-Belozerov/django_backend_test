ab221ac09ba8710a2caefdac705f2bbff312a0a3
alex <belaz.lug@yandex.ru>
2022-05-11 20:56:44 +0300

    Созданы несколько тестовых обьектов модели

--- wicker/store/tests/test_api.py
+++ wicker/store/tests/test_api.py
@@ -1,8 +1,14 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
+from store.models import Wicker
+
 
 class WickerApiTestCase(APITestCase):
 
     def test_get(self):
+        wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500)
+        wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815)
         url = reverse('wicker-list')
+        response = self.client.get(url)
+
