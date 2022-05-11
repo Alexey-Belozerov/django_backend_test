7ad5d9dc212e112d928362d622e4b37729cf9c86
alex <belaz.lug@yandex.ru>
2022-05-11 22:56:56 +0300

    Добавлена функция setUp в тест API, перенесены в нее созданные объекты, добавлено поле автора

--- wicker/store/tests/test_api.py
+++ wicker/store/tests/test_api.py
@@ -7,12 +7,14 @@ from store.serializers import WickerSerializer
 
 
 class WickerApiTestCase(APITestCase):
+    def SetUp(self):
+        self.wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500, author_name='Author 1')
+        self.wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815, author_name='Author 2')
+        self.wicker_3 = Wicker.objects.create(name='Test wicker 3', price=2815, author_name='Author 3')
 
     def test_get(self):
-        wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500)
-        wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815)
         url = reverse('wicker-list')
         response = self.client.get(url)
-        serializer_data = WickerSerializer([wicker_1, wicker_2], many=True).data
+        serializer_data = WickerSerializer([self.wicker_1, self.wicker_2], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
