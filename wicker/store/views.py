5004db1827532a503108a6166073f23f06487968
alex <belaz.lug@yandex.ru>
2022-05-11 19:39:41 +0300

    Создано представление для модели

--- wicker/store/views.py
+++ wicker/store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from store.models import Wicker
+from store.serializers import WickerSerializer
+
+
+class WickerViewSet(ModelViewSet):
+    queryset = Wicker.objects.all()
+    serializer_class = WickerSerializer
+
