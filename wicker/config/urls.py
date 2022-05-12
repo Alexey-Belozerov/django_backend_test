e8753084b63f3744c0460e7d03379e776a910223
alex <belaz.lug@yandex.ru>
2022-05-12 15:19:43 +0300

    Прописан url для представления

--- wicker/config/urls.py
+++ wicker/config/urls.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 # from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from store.views import WickerViewSet
+from store.views import WickerViewSet, auth
 
 router = SimpleRouter()
 router.register(r'wicker', WickerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('', include('social_django.urls', namespace='social'))
+    re_path('', include('social_django.urls', namespace='social')),
+    path('auth/', auth)
 ]
 
 urlpatterns += router.urls
