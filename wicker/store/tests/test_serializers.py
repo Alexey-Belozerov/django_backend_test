5a05680e056a61b0d7591f9ab4e38e4bb87859be
alex <belaz.lug@yandex.ru>
2022-05-12 16:25:37 +0300

    Добавили в test_serializer поле 'author_name'

--- wicker/store/tests/test_serializers.py
+++ wicker/store/tests/test_serializers.py
@@ -7,19 +7,21 @@ from store.serializers import WickerSerializer
 class WickerSerializerTestCase(TestCase):
 
     def test_ok(self):
-        wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500)
-        wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815)
+        wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500, author_name='Author 1')
+        wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815, author_name='Author 2')
         data = WickerSerializer([wicker_1, wicker_2], many=True).data
         expected_data = [
             {
                 'id': wicker_1.id,
                 'name': 'Test wicker 1',
-                'price': '1500.00'
+                'price': '1500.00',
+                'author_name': 'Author 1'
             },
             {
                 'id': wicker_2.id,
                 'name': 'Test wicker 2',
-                'price': '2815.00'
+                'price': '2815.00',
+                'author_name': 'Author 2'
             },
         ]
         self.assertEqual(expected_data, data)
