c6debe51b61c0ca3f4406aaa8e1d653dccbec584
alex <belaz.lug@yandex.ru>
2022-05-12 13:42:48 +0300

    Добавлен тест filter по данным price

--- wicker/store/tests/test_api.py
+++ wicker/store/tests/test_api.py
@@ -7,14 +7,21 @@ from store.serializers import WickerSerializer
 
 
 class WickerApiTestCase(APITestCase):
-    def SetUp(self):
+    def setUp(self):
         self.wicker_1 = Wicker.objects.create(name='Test wicker 1', price=1500, author_name='Author 1')
-        self.wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815, author_name='Author 2')
-        self.wicker_3 = Wicker.objects.create(name='Test wicker 3', price=2815, author_name='Author 3')
+        self.wicker_2 = Wicker.objects.create(name='Test wicker 2', price=2815, author_name='Author 1')
+        self.wicker_3 = Wicker.objects.create(name='Test wicker 3', price=2815, author_name='Author 2')
 
     def test_get(self):
         url = reverse('wicker-list')
         response = self.client.get(url)
-        serializer_data = WickerSerializer([self.wicker_1, self.wicker_2], many=True).data
+        serializer_data = WickerSerializer([self.wicker_1, self.wicker_2, self.wicker_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_filter(self):
+        url = reverse('wicker-list')
+        response = self.client.get(url, data={'price': 2815})
+        serializer_data = WickerSerializer([self.wicker_2, self.wicker_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
