175ba53b240fd76abbffb6645d3b204d3c09e442
alex <belaz.lug@yandex.ru>
2022-05-13 20:33:10 +0300

    Расширим права. Сделаем refactor-rename переименуем класс добавив Staff

--- wicker/store/views.py
+++ wicker/store/views.py
@@ -4,14 +4,14 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Wicker
-from store.permissions import IsOwnerOrReadOnly
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import WickerSerializer
 
 
 class WickerViewSet(ModelViewSet):
     queryset = Wicker.objects.all()
     serializer_class = WickerSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filter_fields = ['price']
     search_fields = ['name', 'author_name']
