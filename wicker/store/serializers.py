d612ccde5bc5668ef770f839673d8ad82d94a77d
alex <belaz.lug@yandex.ru>
2022-05-14 20:12:18 +0300

    Создадим API. Создадим сериализатор, пропишем нужные поля

--- wicker/store/serializers.py
+++ wicker/store/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Wicker
+from store.models import Wicker, UserWickerRelation
 
 
 class WickerSerializer(ModelSerializer):
@@ -8,3 +8,10 @@ class WickerSerializer(ModelSerializer):
         model = Wicker
         fields = '__all__'
         # fields = ['id', 'name', 'price', 'author_name', 'owner']
+
+
+class UserWickerRelationSerializer(ModelSerializer):
+    class Meta:
+        model = UserWickerRelation
+        fields = '__all__'
+        fields = ['wicker', 'like', 'in_bookmarks', 'rate']
