3888088cd512475a93931840d3d4afcb82071a1b
alex <belaz.lug@yandex.ru>
2022-05-14 20:48:59 +0300

    Пропишем url для представления

--- wicker/config/urls.py
+++ wicker/config/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include, re_path
 from django.contrib import admin
 from rest_framework.routers import SimpleRouter
 
-from store.views import WickerViewSet, auth
+from store.views import WickerViewSet, auth, UserWickerRelationViewSet
 
 router = SimpleRouter()
 router.register(r'wicker', WickerViewSet)
+router.register(r'wicker_relation', UserWickerRelationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
